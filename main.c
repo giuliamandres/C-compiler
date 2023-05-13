e1293f201f6323651eb3d155ef9de85b984c5fff
giuliamandres <93037838+giuliamandres@users.noreply.github.com>
2023-05-13 15:45:50 +0300

    the main function

---
+++ main.c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+//#include "lexical_analyzer.c"
+#include "syntactic_analyzer.c"
+
+
+int main(){
+    FILE *f = fopen("file.txt", "r");
+    //char *buffer = 0;
+    if(f == NULL)
+        printf("Can t open the file");
+    else{
+        fseek(f, 0, SEEK_END);
+        long length = ftell(f);
+        printf("length %ld\n", length);
+        fseek(f, 0, SEEK_SET);
+        buffer = malloc(length);
+        if(buffer){
+            fread(buffer, 1, length, f);
+            printf("%s\n", buffer);
+        }
+        fclose(f);
+    }
+
+    int token = getNextToken();
+    printf("token code %d\n", token);
+    while (token != 1){
+        token = getNextToken();
+        printf("token code %d\n", token);
+    }
+    showTokens();
+    //freeMemory();
+    printTk();
+
+}
